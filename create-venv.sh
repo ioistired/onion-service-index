f4e29353d302def36dd136073aa3bb00dbd25592
GI_Jack <GI_Jack@hackermail.com>
2020-01-14 04:51:44 -0500

    There is a special place in hell for those who tab space python.
    Read. Understand. Post.
    https://www.python.org/dev/peps/pep-0008/

--- create-venv.sh
+++ create-venv.sh
@@ -2,9 +2,9 @@
 VENV=./venv
 if ! mkdir -v $VENV
 then
-	echo $VENV seems to alrady exist. If you\'re having trouble, consider
-	echo deleting it before running this script again.
-	exit 1
+  echo $VENV seems to alrady exist. If you\'re having trouble, consider
+  echo deleting it before running this script again.
+  exit 1
 fi
 virtualenv $VENV
 pip3 install flask
